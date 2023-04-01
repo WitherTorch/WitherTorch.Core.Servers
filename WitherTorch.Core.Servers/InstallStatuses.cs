b54890ab30cca1d29dcc1b9227fdbe8bc1ae510a
Error 404 <36844888+new1271@users.noreply.github.com>
2023-04-01 22:50:07 +0800

    將部分類別從 WitherTorch.Core 遷移至 WitherTorch.Core.Servers

---
+++ WitherTorch.Core.Servers/InstallStatuses.cs
@@ -0,0 +1,39 @@
+﻿namespace WitherTorch.Core.Servers
+{
+    public class SpigotBuildToolsStatus : ProcessStatus
+    {
+        public enum ToolState
+        {
+            Initialize,
+            Update,
+            Build
+        }
+
+        private ToolState _state;
+
+        public ToolState State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                _state = value;
+                OnUpdated();
+            }
+        }
+
+        public SpigotBuildToolsStatus(ToolState state, double percentage) : base(percentage)
+        {
+            State = state;
+        }
+    }
+
+    public class FabricInstallerStatus : SpigotBuildToolsStatus
+    {
+        public FabricInstallerStatus(ToolState state, double percentage) : base(state, percentage)
+        {
+        }
+    }
+}
