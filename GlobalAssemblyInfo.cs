268d23276590567e28b1e9979a728f1868d99ee8
Error 404 <36844888+new1271@users.noreply.github.com>
2023-04-01 22:45:34 +0800

    加入基礎資訊

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("WitherTorch 製作組")]
+[assembly: AssemblyProduct("WitherTorch")]
+[assembly: AssemblyCopyright("Copyright © WitherTorch 製作組 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.8.1.1")]
