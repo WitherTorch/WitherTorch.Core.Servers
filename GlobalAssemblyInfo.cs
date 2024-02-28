88d93ab15257652e4c1ec5fd9fd9f767b4bc6e22
Error 404 <36844888+new1271@users.noreply.github.com>
2024-02-29 01:27:49 +0800

    Updated Submodule

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,8 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("WitherTorch 製作組")]
 [assembly: AssemblyProduct("WitherTorch")]
 [assembly: AssemblyCopyright("Copyright © WitherTorch 製作組 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.8.1.4")]
+[assembly: AssemblyVersion("1.8.2.5")]
+
+//Used for NGen pre-linking
+[assembly: Dependency("System.IO.Compression", LoadHint.Always)]
+[assembly: Dependency("Newtonsoft.Json", LoadHint.Always)]
+[assembly: Dependency("YamlDotNet", LoadHint.Always)]
+[assembly: Dependency("WitherTorch.Core", LoadHint.Always)]
---
+++ WitherTorch.Core.Servers/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using System.Reflection;
+[assembly: AssemblyTitle("WitherTorch.Core.Server")]
+[assembly: AssemblyDescription("Default Server Software Implementations for WitherTorch.Core")]
