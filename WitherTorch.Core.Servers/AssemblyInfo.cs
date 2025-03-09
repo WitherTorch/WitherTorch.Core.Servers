6a89b348fd58b2f8695b466f6e24701e2a49265e
米茶 <new1271@outlook.com>
2025-03-09 19:45:48 +0800

    更新套件、專案目標和版本號

--- WitherTorch.Core.Servers/AssemblyInfo.cs
+++ WitherTorch.Core.Servers/AssemblyInfo.cs
@@ -1,3 +1,18 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
 [assembly: AssemblyTitle("WitherTorch.Core.Server")]
-[assembly: AssemblyDescription("Default Server Software Implementations for WitherTorch.Core")]
+[assembly: AssemblyDescription("WitherTorch.Core 的預設伺服器實現集")]
+
+[assembly: AssemblyCompany("WitherTorch 製作組")]
+[assembly: AssemblyProduct("WitherTorch")]
+[assembly: AssemblyCopyright("Copyright © WitherTorch 製作組 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.8.3.0")]
+[assembly: AssemblyFileVersion("1.8.3.9")]
+
+//Used for NGen pre-linking
+[assembly: Dependency("System.Text.Json", LoadHint.Always)]
+[assembly: Dependency("YamlDotNet", LoadHint.Always)]
+[assembly: Dependency("WitherTorch.Core", LoadHint.Always)]
