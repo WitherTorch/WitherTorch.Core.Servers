a8937a751be923d3c555b9cc1f46314676419813
米茶 <new1271@outlook.com>
2026-04-26 20:26:18 +0800

    更新版權資訊

--- WitherTorch.Core.Servers/AssemblyInfo.cs
+++ WitherTorch.Core.Servers/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("WitherTorch.Core.Server")]
@@ -6,11 +6,11 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("WitherTorch 製作組")]
 [assembly: AssemblyProduct("WitherTorch")]
-[assembly: AssemblyCopyright("Copyright © WitherTorch 製作組 2025")]
+[assembly: AssemblyCopyright("Copyright © WitherTorch 製作組 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.8.4.0")]
-[assembly: AssemblyFileVersion("1.8.4.10")]
+[assembly: AssemblyFileVersion("1.8.4.11")]
 
 //Used for NGen pre-linking
 [assembly: Dependency("System.Text.Json", LoadHint.Always)]
